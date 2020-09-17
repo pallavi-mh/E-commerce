c862abeaba5e68f962e18781903d0959839b3bb9
pallavimh <pallavi@graniteriverlabs.in>
2020-09-17 22:14:16 +0530

    added functionality to navigate menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>hats page</h1>
+  </div>
+)
 
 
 function App() {
   return (
-    <div><HomePage /></div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
